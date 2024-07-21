116350d66763b2447fe059525875888a25adf636
shreyaSrivastava78 <shreyashreya1986@gmail.com>
2024-07-21 15:30:07 +0530

    Add files via upload

---
+++ SCT_WD_1/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function() {
+            tabs.forEach(t => t.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+});
